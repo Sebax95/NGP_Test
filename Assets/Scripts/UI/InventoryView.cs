a48cdadb730b197e0d6f2f95ece108e0ce6f04aa
Sebax95 <orrillo.sebastian@gmail.com>
2025-03-09 03:11:23 -0300

    Add inventory animations and refactor InventoryController
    Introduced animations for opening, closing, and selecting items in the inventory using DOTween. Refactored InventoryController to delegate UI-related actions to InventoryView for better separation of concerns. Improved code clarity by renaming variables to follow consistent naming conventions.

--- Assets/Scripts/UI/InventoryView.cs
+++ Assets/Scripts/UI/InventoryView.cs
@@ -1,7 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
 
 public class InventoryView: MonoBehaviour
 {
     
+    [SerializeField]
+    private Transform inventoryContainer;
+    [SerializeField] 
+    private ItemDescription _itemDescription;
 
+    private Vector3 _startPositionDescription;
+    private Vector3 _startPositionInventory;
+    private void Start()
+    {
+        _startPositionInventory = inventoryContainer.position;
+    }
+
+    public void AnimationDescription(Action onComplete) => _itemDescription.transform
+        .DOMoveX(_startPositionDescription.x + 550, .2f)
+        .SetEase(Ease.InOutSine)
+        .OnComplete(() => onComplete?.Invoke());
+    
+    public void OpenInventory(Action onComplete)
+    {
+        inventoryContainer.DOMoveX(_startPositionInventory.x + 2365,.2f).SetEase(Ease.InOutSine).OnComplete(() =>
+        {
+            _startPositionDescription = _itemDescription.transform.position;
+            onComplete?.Invoke();
+        });
+    }
+    public void CloseInventory(Action onComplete)
+    {
+        inventoryContainer.DOMoveX(_startPositionInventory.x, .2f).SetEase(Ease.InOutSine).OnComplete(() =>
+        {
+            _itemDescription.transform.position = new Vector3(_startPositionDescription.x - 2365, _startPositionDescription.y, _startPositionDescription.z); 
+            onComplete?.Invoke();
+        });
+
+    }
 }
