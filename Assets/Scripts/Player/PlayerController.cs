0afc71cdc59128d874efbfde02942960c549e766
Sebax95 <orrillo.sebastian@gmail.com>
2025-03-09 02:00:08 -0300

    Added Inventory UI

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,12 +9,18 @@ public class PlayerController: IController
     }
 
 
-    public void CheckInputs()
+    public void CheckMovementInputs()
     {
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         _player.MovePlayer(direction.normalized);
+    }
+
+    public void CheckInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            _player.PickUpItem();
 
         if (Input.GetKeyDown(KeyCode.E))
-            _player.PickUpItem();
+            _player.OpenInventory();
     }
 }
---
+++ Assets/Scripts/UI/ItemDescription.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemDescription: MonoBehaviour
+{
+    public Item item;
+    public Image imageSprite;
+    public TextMeshProUGUI textName;
+    public TextMeshProUGUI textDescription;
+
+    private void Start() => EmptySlot();
+
+    public void SetItem(Item itemValue)
+    {
+        item = itemValue;
+        UpdateInfo();
+    }
+    
+    private void SetSprite()
+    {
+        imageSprite.sprite = item.sprite;
+        imageSprite.color = Color.white;
+    }
+    private void SetText() => textName.text = item.name;
+    private void SetDescription() => textDescription.text = item.description;
+
+    public void EmptySlot()
+    {
+        item = null;
+        imageSprite.sprite = null;
+        imageSprite.color = new Color(0,0,0,0);
+        textName.text = "";
+        textDescription.text = "";
+    }
+    private void UpdateInfo()
+    {
+        SetSprite();
+        SetText();
+        SetDescription();
+    }
+
+    
+}
